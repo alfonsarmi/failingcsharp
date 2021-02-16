fa72986013d62971af78240d83b7255d2f83b369
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2021-02-16 13:49:17 +0100

    First Commit

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespa
+					//this is a test 1
+					var v = 1;
+					
+					if (a == 2)
+					{
+						//this is a test 2
+						var v = 1;
+					}
+				}
+				
+				if (a == 2)
+				{
+					//this is a test 3
+					var v = 1;
+				}
+			}
+
+			if (a == 1) 
+			{
+				if (a == 1 && b == 2 || b == 1) 
+				{
+					if (b == 2)  
+					{
+						var p = 1;
+						switch (b) 
+						{
+							case 1: var z = 1;
+							break;
+				
+							case 2: var w = 2;
+							break;
+
+							default: var o = 3;
+							break;
+
+						}
+
+					}
+				}
+
+			}
+		}
+    }
+}
